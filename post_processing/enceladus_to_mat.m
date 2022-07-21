e6219ba11bd77bed62d54226f01296eb134e6436
cshsgy <shc@nuwa.gps.caltech.edu>
2022-07-20 22:52:19 -0700

    update

--- post_processing/enceladus_to_mat.m
+++ post_processing/enceladus_to_mat.m
@@ -4,7 +4,7 @@ function data_to_store = enceladus_to_mat(folder_name)
     data_to_store = {};
     tmp = strrep(folder_name,'/','');
     tmp = str2double(tmp(end));
-    load(strcat(folder_name),'/run_results.mat','width_rec','phi_rec','time_rec');
+    load(strcat(folder_name,'/run_results.mat'),'width_rec','phi_rec','time_rec');
     data_to_store{end+1} = [5000*2^tmp min(width_rec) max(width_rec)/min(width_rec)];
     [phi,t] = accum_last_days(118800,time_rec,val_rec,15)
     data_to_store{end+1} = phi;
