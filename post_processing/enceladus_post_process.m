601308aab11b7b08b54e4c01032b61e8c406585d
Sihe Chen <sihechen@login1.cm.cluster>
2022-07-23 18:00:05 -0700

    update on enceladus post

--- post_processing/enceladus_post_process.m
+++ post_processing/enceladus_post_process.m
@@ -6,7 +6,7 @@ function enceladus_post_process(folder_list)
         filn = strcat(folder_list{i},'/run_results.mat');
         load(filn,'Period','time_rec','phi_rec','width_rec');
         [phi_day,time_day] = accum_last_days(Period,time_rec,smooth(phi_rec.*width_rec*520000,100),5);
-        phi_day = phi_day;
+        phi_day = phi_day / max(phi_day);
         plot(time_day,smooth(phi_day,100),'linewidth',2); % 
         tmp = folder_list{i};
         tmp = tmp(end-5:end);
@@ -14,7 +14,7 @@ function enceladus_post_process(folder_list)
     end
     xlabel('Time (Day)');
     ylabel('Mass Flux (kg s^{-1})');
-    ylim([0 500]);
+    % ylim([0 500]);
     legend(legends,'Location','bestoutside');
     hold off;
     saveas(gcf,strcat(folder_list{1}(end-5:end),'.png'));
